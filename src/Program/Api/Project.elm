b16fb2191227c24d9eef077f678245d87270ebc0
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-08-14 23:15:17 +0200

    next

--- src/Program/Api/Project.elm
+++ src/Program/Api/Project.elm
@@ -3,21 +3,27 @@ module Program.Api.Project exposing (..)
 import Dict.Any
 import Id
 import Id.Random
+import Json.Decode
+import Json.Encode
 import Program.Api.Declaration
-import Program.Api.Meta
+import Program.Api.User
 import Task
+import Time
 
 
 type alias Project =
     { name : String
     , description : String
+    , data : Json.Decode.Value
 
     --
     , declarations : Dict.Any.Dict (Id.Id Program.Api.Declaration.Declaration) Program.Api.Declaration.Declaration
     , activeDeclaration : Maybe (Id.Id Program.Api.Declaration.Declaration)
 
     --
-    , meta : Program.Api.Meta.Meta
+    , author : Maybe (Id.Id Program.Api.User.User)
+    , created : Time.Posix
+    , modified : Time.Posix
     }
 
 
@@ -29,10 +35,13 @@ new =
             , Project
                 "Project"
                 ""
+                (Json.Encode.object [])
                 Dict.Any.empty
                 Nothing
+                Nothing
+                x2
                 x2
             )
         )
         Id.Random.generate
-        Program.Api.Meta.new
+        Time.now
